e9ba6f888c1f93549f1d65be63f0328f8397c812
Hannah Masila <hannahmasila@gmail.com>
2023-02-09 07:24:19 +0300

    more regex challenges

--- coding-game/regex_challenges.rb
+++ coding-game/regex_challenges.rb
@@ -8,3 +8,17 @@ str.scan(/[A-Z]/).length
 # sum of digits in a string
 
 str.scan(/[0-9]/).length
+
+# *********************
+# convert consecutive v's to w
+
+str.gsub(/v+/, 'w')
+
+# **************
+# Write ruby code to return only uppercase characters from a string
+
+str.gsub(/[^A-Z]/, '')
+
+# or
+
+str.chars.select{ |c| c.ord >= 65 && c.ord <= 90 }.join
