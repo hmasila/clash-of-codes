77e54ddb026e6f61de8368eebac69ac8d7b0a09c
Hannah Masila <hannahmasila@gmail.com>
2023-03-09 22:39:48 -0400

    regex challenges

---
+++ coding-game/regex_challenges.rb
@@ -0,0 +1,10 @@
+# *********************
+# sum of uppercase characters in a string
+
+str.scan(/[A-Z]/).length
+
+# **********************
+
+# sum of digits in a string
+
+str.scan(/[0-9]/).length
